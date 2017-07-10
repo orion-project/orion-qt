1a84c94d5b47522399737f622156ea87d32dd2f8
Nikolay Chunosov <nikolay.chunosov@xored.com>
2017-07-10 13:17:02 +0700

    Layout::makeWidget

--- helpers/OriLayouts.h
+++ helpers/OriLayouts.h
@@ -81,6 +81,7 @@ public:
     LayoutBox& setMargin(int value) { _layout->setMargin(value); return *this; }
     LayoutBox& setSpacing(int value) { boxLayout()->setSpacing(value); return *this; }
     LayoutBox& useFor(QWidget* parent) { parent->setLayout(_layout); return *this; }
+    QWidget* makeWidget() { auto w = new QWidget; w->setLayout(_layout); return w; }
 
     QBoxLayout* boxLayout() const { return (QBoxLayout*)_layout; }
 };
