a63fab32868cfa7ba99c86e035a1b74ba8b42e4f
Nikolay Chunosov <N.Chunosov@yandex.ru>
2018-06-25 22:49:40 +0700

    new methods for Ori::Filter

--- core/OriFilter.h
+++ core/OriFilter.h
@@ -12,13 +12,14 @@ public:
 
     Filter(std::initializer_list<TCondition*> conditions): _conditions(conditions) {}
 
-    ~Filter()
+    virtual ~Filter()
     {
         for (TCondition* condition : _conditions)
             delete condition;
     }
 
-    bool check(TTarget target)
+    /// The functions checks if a target satisfies to all conditions of this filter.
+    bool check(TTarget target) const
     {
         for (TCondition* condition : _conditions)
             if (!condition->check(target))
@@ -26,7 +27,37 @@ public:
         return true;
     }
 
-    void append(TCondition* condition) { _conditions.push_back(condition); }
+    /// The function appends a condition into conditions list of this filter.
+    /// The filter takes ownership of the condition.
+    void append(TCondition* condition)
+    {
+        _conditions.push_back(condition);
+    }
+
+    /// The function returns the number of object in a list satisfying to all conditions of this filter.
+    template <typename TContainer>
+    int count(const TContainer& container) const
+    {
+        int count = 0;
+        typename TContainer::const_iterator it;
+        for (it = container.begin(); it != container.end(); it++)
+            if (check(*it))
+                count++;
+        return count;
+    }
+
+    /// The function returns a container of the same type as the input container,
+    /// but containing only objects satisfying to all conditions of this filter.
+    template <typename TContainer>
+    TContainer filter(const TContainer& container) const
+    {
+        TContainer result;
+        typename TContainer::const_iterator it;
+        for (it = container.begin(); it != container.end(); it++)
+            if (check(*it))
+                result.insert(result.end(), *it);
+        return result;
+    }
 
 protected:
     std::vector<TCondition*> _conditions;
--- tests/ori_test_Filter.cpp
+++ tests/ori_test_Filter.cpp
@@ -5,16 +5,16 @@ namespace Ori {
 namespace Tests {
 namespace FilterTests {
 
-class TestCondition {
+class MockTestCondition {
 public:
     QString id;
     Ori::Testing::TestBase* test;
     bool result;
 
-    TestCondition(const QString& id, Ori::Testing::TestBase *test, bool result = true)
+    MockTestCondition(const QString& id, Ori::Testing::TestBase *test, bool result = true)
         : id(id), test(test), result(result) {}
 
-    ~TestCondition()
+    ~MockTestCondition()
     {
         TEST_LOG("~TestCondition() " + id)
         test->data().insert(id, "deleted");
@@ -28,14 +28,12 @@ public:
     }
 };
 
-typedef Filter<QString, TestCondition> TestFilter;
-
 TEST_METHOD(destructor_must_delete_all_conditions)
 {
     const int conditionCount = 5;
-    auto filter = new TestFilter;
+    auto filter = new Filter<QString, MockTestCondition>;
     for (int i = 0; i < conditionCount; i++)
-        filter->append(new TestCondition(QString::number(i), test));
+        filter->append(new MockTestCondition(QString::number(i), test));
 
     // when
     delete filter;
@@ -47,9 +45,9 @@ TEST_METHOD(destructor_must_delete_all_conditions)
 
 TEST_METHOD(check_must_call_all_conditions)
 {
-    TestFilter filter({
-        new TestCondition("1", test),
-        new TestCondition("2", test)
+    Filter<QString, MockTestCondition> filter({
+        new MockTestCondition("1", test),
+        new MockTestCondition("2", test)
     });
     const QString checkValue("test");
 
@@ -64,9 +62,9 @@ TEST_METHOD(check_must_call_all_conditions)
 
 TEST_METHOD(check_must_return_false_when_one_condition_failed)
 {
-    TestFilter filter({
-        new TestCondition("1", test, true),
-        new TestCondition("2", test, false)
+    Filter<QString, MockTestCondition> filter({
+        new MockTestCondition("1", test, true),
+        new MockTestCondition("2", test, false)
     });
 
     // when
@@ -76,12 +74,87 @@ TEST_METHOD(check_must_return_false_when_one_condition_failed)
     ASSERT_IS_FALSE(result)
 }
 
+class IntTestCondition
+{
+public:
+    IntTestCondition(int ref): _ref(ref) {}
+    bool check(int val) const { return val == _ref; }
+private:
+    int _ref;
+};
+
+TEST_METHOD(count_must_return_number_of_items_in_container)
+{
+    Filter<int, IntTestCondition> filter({
+        new IntTestCondition(100)
+    });
+
+    std::vector<int> vals({100, 100, 200, 300});
+
+    // when
+    int result = filter.count(vals);
+
+    // then
+    ASSERT_EQ_INT(result, 2)
+}
+
+TEST_METHOD(count_must_return_zero_when_no_conditions_satisfied)
+{
+    Filter<int, IntTestCondition> filter({
+        new IntTestCondition(100)
+    });
+
+    std::vector<int> vals({400, 400, 200, 300});
+
+    // when
+    int result = filter.count(vals);
+
+    // then
+    ASSERT_EQ_INT(result, 0)
+}
+
+TEST_METHOD(filter_must_return_part_of_container)
+{
+    Filter<int, IntTestCondition> filter({
+        new IntTestCondition(100)
+    });
+
+    std::vector<int> vals({100, 100, 200, 300});
+
+    // when
+    auto result = filter.filter(vals);
+
+    // then
+    ASSERT_EQ_INT(result.size(), 2)
+    ASSERT_EQ_INT(result[0], 100)
+    ASSERT_EQ_INT(result[1], 100)
+}
+
+TEST_METHOD(filter_must_return_empty_container_when_no_conditions_satisfied)
+{
+    Filter<int, IntTestCondition> filter({
+        new IntTestCondition(100)
+    });
+
+    std::vector<int> vals({400, 400, 200, 300});
+
+    // when
+    auto result = filter.filter(vals);
+
+    // then
+    ASSERT_IS_TRUE(result.empty())
+}
+
 //------------------------------------------------------------------------------
 
 TEST_GROUP("Filter",
     ADD_TEST(destructor_must_delete_all_conditions),
     ADD_TEST(check_must_call_all_conditions),
-    ADD_TEST(check_must_return_false_when_one_condition_failed)
+    ADD_TEST(check_must_return_false_when_one_condition_failed),
+    ADD_TEST(count_must_return_number_of_items_in_container),
+    ADD_TEST(count_must_return_zero_when_no_conditions_satisfied),
+    ADD_TEST(filter_must_return_part_of_container),
+    ADD_TEST(filter_must_return_empty_container_when_no_conditions_satisfied),
 )
 
 } // namespace TemplatesTests
