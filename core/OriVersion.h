17829fe143cebd19618bbe05a103d9a233a42332
Nikolay Chunosov <N.Chunosov@yandex.ru>
2019-05-01 12:41:14 +0700

    use patch number in version

--- core/OriVersion.h
+++ core/OriVersion.h
@@ -2,82 +2,126 @@
 #define ORI_VERSION_H
 
 #include <QString>
+#include <QDebug>
 
 namespace Ori {
 
 class Version
 {
 public:
-    Version(): _major(0), _minor(0)
+    Version(): _major(0), _minor(0), _patch(0)
     {}
 
-    Version(int major, int minor): _major(major), _minor(minor)
+    Version(int major, int minor = 0, int patch = 0): _major(major), _minor(minor), _patch(patch)
     {}
 
     Version(const QString& ver)
     {
-        _major = ver.section('.', 0, 0).toInt();
-        _minor = ver.section('.', 1, 1).toInt();
+        bool majorOk = true, minorOk = true, patchOk = true;
+        auto parts = ver.split('.');
+        if (parts.size() >= 3)
+        {
+            _major = parts.at(0).toInt(&majorOk);
+            _minor = parts.at(1).toInt(&minorOk);
+            _patch = parts.at(2).toInt(&patchOk);
+        }
+        else if (parts.size() == 2)
+        {
+            _major = parts.at(0).toInt(&majorOk);
+            _minor = parts.at(1).toInt(&minorOk);
+            _patch = 0;
+        }
+        else
+        {
+            _major = ver.toInt(&majorOk);
+            _minor = 0;
+            _patch = 0;
+        }
+        if (!majorOk || !minorOk || !patchOk)
+        {
+            _major = 0;
+            _minor = 0;
+            _patch = 0;
+        }
     }
 
     #undef major // disable GNU C Lib macro
     #undef minor // disable GNU C Lib macro
     int major() const { return _major; }
     int minor() const { return _minor; }
+    int patch() const { return _patch; }
 
-    bool match(int major, int minor) const
+    bool isValid() const
     {
-        return _major == major && _minor == minor;
+        return _major > 0 || _minor > 0 || _patch > 0;
     }
 
-    bool less(int major, int minor) const
+    bool match(int major, int minor = 0, int patch = 0) const
     {
-        return (_major < major) || (_major == major && _minor < minor);
+        return _major == major && _minor == minor && _patch == patch;
     }
 
-    bool more(int major, int minor) const
+    bool less(int major, int minor = 0, int patch = 0) const
     {
-        return (_major > major) || (_major == major && _minor > minor);
+        return (_major < major) ||
+               (_major == major && _minor < minor) ||
+               (_major == major && _minor == minor && _patch < patch);
     }
 
-    QString str() const
+    bool more(int major, int minor = 0, int patch = 0) const
     {
-        return QString("%1.%2").arg(_major).arg(_minor);
+        return (_major > major) ||
+               (_major == major && _minor > minor) ||
+               (_major == major && _minor == minor && _patch > patch);
+    }
+
+    QString str(int parts = 2) const
+    {
+        if (parts <= 1)
+            return QString::number(_major);
+
+        if (parts == 2)
+            return QString("%1.%2").arg(_major).arg(_minor);
+
+        return QString("%1.%2.%3").arg(_major).arg(_minor).arg(_patch);
     }
 
     bool operator == (const Version& other) const
     {
-        return match(other.major(), other.minor());
+        return match(other.major(), other.minor(), other.patch());
     }
 
     bool operator != (const Version& other) const
     {
-        return !match(other.major(), other.minor());
+        return !match(other.major(), other.minor(), other.patch());
     }
 
     bool operator < (const Version& other) const
     {
-        return less(other.major(), other.minor());
+        return less(other.major(), other.minor(), other.patch());
     }
 
     bool operator > (const Version& other) const
     {
-        return more(other.major(), other.minor());
+        return more(other.major(), other.minor(), other.patch());
     }
 
     bool operator <= (const Version& other) const
     {
-        return less(other.major(), other.minor()) || match(other.major(), other.minor());
+        return less(other.major(), other.minor(), other.patch()) ||
+               match(other.major(), other.minor(), other.patch());
     }
 
     bool operator >= (const Version& other) const
     {
-        return more(other.major(), other.minor()) || match(other.major(), other.minor());
+        return more(other.major(), other.minor(), other.patch()) ||
+               match(other.major(), other.minor(), other.patch());
     }
 
 private:
     int _major;
     int _minor;
+    int _patch;
 };
 
 }
--- tests/ori_test_Version.cpp
+++ tests/ori_test_Version.cpp
@@ -7,28 +7,68 @@ namespace VersionTests {
 
 //------------------------------------------------------------------------------
 
-TEST_METHOD(constructors)
+TEST_METHOD(constructor_int)
 {
     Version v0;
-    ASSERT_IS_TRUE(v0.match(0, 0))
-    TEST_LOG(v0.str())
+    TEST_LOG(v0.str(3))
+    ASSERT_IS_TRUE(v0.match(0, 0, 0))
 
-    Version v1(3, 4);
-    ASSERT_IS_TRUE(v1.match(3, 4))
-    TEST_LOG(v1.str())
+    Version v1(2);
+    TEST_LOG(v1.str(3))
+    ASSERT_IS_TRUE(v1.match(2, 0, 0))
 
-    Version v2("5.6.7");
-    ASSERT_IS_TRUE(v2.match(5, 6))
-    TEST_LOG(v2.str())
+    Version v2(3, 4);
+    TEST_LOG(v2.str(3))
+    ASSERT_IS_TRUE(v2.match(3, 4, 0))
+
+    Version v3(5, 6, 7);
+    TEST_LOG(v3.str(3))
+    ASSERT_IS_TRUE(v3.match(5, 6, 7))
+}
+
+TEST_METHOD(constructor_str)
+{
+    Version v1("2");
+    TEST_LOG(v1.str(3))
+    ASSERT_IS_TRUE(v1.match(2, 0, 0))
+
+    Version v2("3.4");
+    TEST_LOG(v2.str(3))
+    ASSERT_IS_TRUE(v2.match(3, 4, 0))
+
+    Version v3("5.6.7");
+    TEST_LOG(v3.str())
+    ASSERT_IS_TRUE(v3.match(5, 6, 7))
+
+    Version v4("8.9.2.3");
+    TEST_LOG(v4.str())
+    ASSERT_IS_TRUE(v4.match(8, 9, 2))
+}
+
+TEST_METHOD(constructor_str_invaid)
+{
+    Version v0("this not a version");
+    ASSERT_IS_TRUE(v0.match(0))
+
+    Version v1("this not a version.1.2");
+    ASSERT_IS_TRUE(v1.match(0))
+
+    Version v2("1.this not a version.2");
+    ASSERT_IS_TRUE(v2.match(0))
+
+    Version v3("1.2.this not a version");
+    ASSERT_IS_TRUE(v3.match(0))
 }
 
 TEST_METHOD(more_and_less)
 {
-    Version v1(1, 5);
-    ASSERT_IS_TRUE(v1.less(2, 0))
-    ASSERT_IS_TRUE(v1.less(1, 6))
-    ASSERT_IS_TRUE(v1.more(1, 1))
-    ASSERT_IS_TRUE(v1.more(0, 4))
+    Version v2(1, 5, 5);
+    ASSERT_IS_TRUE(v2.less(2, 1, 1))
+    ASSERT_IS_TRUE(v2.less(1, 6, 1))
+    ASSERT_IS_TRUE(v2.less(1, 5, 7))
+    ASSERT_IS_TRUE(v2.more(0, 6, 7))
+    ASSERT_IS_TRUE(v2.more(1, 4, 7))
+    ASSERT_IS_TRUE(v2.more(1, 5, 4))
 }
 
 TEST_METHOD(operators)
@@ -46,12 +86,43 @@ TEST_METHOD(operators)
     ASSERT_IS_TRUE(v3 >= v1)
 }
 
+TEST_METHOD(isValid)
+{
+    Version v0;
+    ASSERT_IS_FALSE(v0.isValid());
+
+    Version v1(1);
+    ASSERT_IS_TRUE(v1.isValid());
+
+    Version v2(0, 1);
+    ASSERT_IS_TRUE(v2.isValid());
+
+    Version v3(0, 0, 1);
+    ASSERT_IS_TRUE(v3.isValid());
+}
+
+TEST_METHOD(str)
+{
+    Version v("5.6.7");
+    ASSERT_EQ_STR(v.str(-1), "5")
+    ASSERT_EQ_STR(v.str(0), "5")
+    ASSERT_EQ_STR(v.str(1), "5")
+    ASSERT_EQ_STR(v.str(), "5.6")
+    ASSERT_EQ_STR(v.str(2), "5.6")
+    ASSERT_EQ_STR(v.str(3), "5.6.7")
+    ASSERT_EQ_STR(v.str(5), "5.6.7")
+}
+
 //------------------------------------------------------------------------------
 
 TEST_GROUP("Version",
-    ADD_TEST(constructors),
+    ADD_TEST(constructor_int),
+    ADD_TEST(constructor_str),
+    ADD_TEST(constructor_str_invaid),
     ADD_TEST(more_and_less),
-    ADD_TEST(operators)
+    ADD_TEST(operators),
+    ADD_TEST(isValid),
+    ADD_TEST(str)
 )
 
 } // namespace VersionTests
